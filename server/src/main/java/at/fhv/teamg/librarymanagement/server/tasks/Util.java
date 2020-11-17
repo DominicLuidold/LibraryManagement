0e77cfeccc4bd6adfe7886735effb42307b56dd4
Alexander Weber <43758870+alexw-at@users.noreply.github.com>
2020-11-17 12:16:49 +0100

    mr checkstyle give me a dream

--- server/src/main/java/at/fhv/teamg/librarymanagement/server/tasks/Util.java
+++ server/src/main/java/at/fhv/teamg/librarymanagement/server/tasks/Util.java
@@ -4,6 +4,12 @@ import at.fhv.teamg.librarymanagement.server.persistance.entity.Lending;
 import java.time.format.DateTimeFormatter;
 
 public class Util {
+    /**
+     * Creates an overdue lending message.
+     *
+     * @param lending lending entity used to create the message
+     * @return message
+     */
     public static String createOverdueMessage(Lending lending) {
         return new StringBuilder().append("Overdue ")
             .append(lending.getMediumCopy().getMedium().getType().getName())
