1b933a0d6253ed6777639fc0dd31b64d0d497879
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-12-02 14:20:58 +0100

    add openapi yml export

--- server/src/main/java/at/fhv/teamg/librarymanagement/server/rest/Rest.java
+++ server/src/main/java/at/fhv/teamg/librarymanagement/server/rest/Rest.java
@@ -1,9 +1,20 @@
 package at.fhv.teamg.librarymanagement.server.rest;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "LibraryManagement",
+        version = "1.0",
+        description = "REST API for Library Management",
+        contact = @Contact(url = "https://fhv.at", name = "FHV Team G")
+    )
+)
 public class Rest {
     private static final Logger LOG = LogManager.getLogger(Rest.class);
     public static final String ADMIN = "Admin";
