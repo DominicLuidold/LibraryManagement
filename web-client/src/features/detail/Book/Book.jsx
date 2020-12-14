f950910f36607fa146cf09245a3ec28433d56d97
Alexander Weber <43758870+alexw-at@users.noreply.github.com>
2020-12-14 16:16:21 +0100

    add detail slice

--- web-client/src/features/detail/Book/Book.jsx
+++ web-client/src/features/detail/Book/Book.jsx
@@ -1,7 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { loadBookDetail, selectBookDetail } from "../detailSlice";
 
 function Book() {
     let { id } = useParams();
+    const dispatch = useDispatch();
+    dispatch(loadBookDetail(id));
+    const bookDetail = useSelector(selectBookDetail);
+    console.log(bookDetail);
 
     return (
         <h1>Detail of Book {id}</h1>
---
+++ web-client/src/features/detail/detailSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const detailSlice = createSlice({
+    name: "detail",
+    initialState: {
+        book: undefined,
+        dvd: undefined,
+        game: undefined,
+    },
+    reducers: {
+        setBook: (state, action) => {
+            console.log(action);
+            state.book = action.payload;
+        },
+        setDvd: (state, action) => {
+            state.dvd = action.payload;
+        },
+        setGame: (state, action) => {
+            state.game = action.payload;
+        },
+    },
+});
+
+export const { setBook, setDvd, setGame } = detailSlice.actions;
+
+export const loadBookDetail = bookId => dispatch => {
+    let url = `***REMOVED***/detail/book/${bookId}`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => dispatch(setBook(data)));
+}
+
+export const selectBookDetail = state => state.detail.book;
+
+export default detailSlice.reducer;
